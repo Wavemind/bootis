621d62c048ab6305c6656754ddf2f87ef80294b4
Sinan Ucak <ucak.sinan@gmail.com>
2022-12-06 16:38:16 +0100

    added next-i18next for translations

--- front/pages/index.js
+++ front/pages/index.js
@@ -2,6 +2,8 @@
  * The external imports
  */
 import { Heading } from '@chakra-ui/react'
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 /**
  * The internal imports
@@ -9,9 +11,20 @@ import { Heading } from '@chakra-ui/react'
 import { Page } from '/components'
 
 export default function Home() {
+  const { t } = useTranslation('common')
+
   return (
-    <Page title='Bootis'>
-      <Heading as='h1'>Bootis</Heading>
+    <Page title={t('title')}>
+      <Heading as='h1'>{t('title')}</Heading>
     </Page>
   )
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+      // Will be passed to the page component as props
+    },
+  }
 }
