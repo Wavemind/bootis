785a33f394b843ab376b96867da3cc61d252b885
Sinan Ucak <ucak.sinan@gmail.com>
2022-12-07 10:08:48 +0100

    corrected PR comments

--- front/pages/index.js
+++ front/pages/index.js
@@ -10,7 +10,10 @@ import { Page } from '/components'
 
 export default function Home() {
   return (
-    <Page title='Bootis'>
+    <Page
+      title='Bootis'
+      description='The application for roaming disabled people'
+    >
       <Heading as='h1'>Bootis</Heading>
     </Page>
   )
