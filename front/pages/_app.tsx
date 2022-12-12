d7be82aa646e7dd72666e04762f67c063c11da02
Quentin Girard <quentin_girard@hotmail.com>
2022-12-12 13:23:32 +0100

    Setup fronts + sitemap + seo

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -3,18 +3,23 @@
  */
 import { ChakraProvider } from '@chakra-ui/react'
 import { appWithTranslation } from 'next-i18next'
+import { DefaultSeo } from 'next-seo'
 
 /**
  * The internal imports
  */
 import theme from '../lib/theme'
+import Fonts from '../lib/utils/fonts'
 import Layout from '../lib/layouts/default'
+import SEO from '../next-seo.config'
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || (page => <Layout>{page}</Layout>)
 
   return (
     <ChakraProvider theme={theme}>
+      <Fonts />
+      <DefaultSeo {...SEO} />
       {getLayout(<Component {...pageProps} />)}
     </ChakraProvider>
   )
