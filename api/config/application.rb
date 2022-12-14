287fc83d5ed2efd4c238be604237477f2933f984
Manu <emmanuel.barchichat@wavemind.ch>
2022-12-14 09:38:24 +0100

    Fix from PR

--- api/config/application.rb
+++ api/config/application.rb
@@ -10,6 +10,8 @@ module Api
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.time_zone = 'Bern'
+    config.i18n.default_locale = :en
 
     # Configuration for the application, engines, and railties goes here.
     #
