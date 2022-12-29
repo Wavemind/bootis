31e5c8458ee3e687b4642aa7e68c9c573411b3af
Alain Fresco <alain.fresco@gmail.com>
2022-12-29 15:10:54 +0100

    Adding pictures and handling if we're out of activities

--- api/app/models/place.rb
+++ api/app/models/place.rb
@@ -28,16 +28,28 @@ class Place < ActiveRecord::Base
   end
 
   # Will retrive `limit` amout of activities based on the categories and the region we are looking in
-  def self.match_activities(region, categories, limit, excluding = [])
+  def self.match_activities(region, categories, limit, accommocation, excluding = [])
     first = Place.get_activities(excluding,region, categories).sample
+    first = Place.get_activities(excluding,region).sample unless first.present?
+    first = Place.get_activities(excluding).near(accommocation).first unless first.present?
+
     excluding << first
     restaurants = Place.joins(:category).where(region: region).where(categories: {section: 'restaurant'}).near(first).slice(0,2)
     others = Place.get_activities(excluding, region, categories).near(first).distinct(:name).slice(0, limit - 1)
+
+    # If we don't find we don't care about categories
+    if others.count < limit - 1
+      others +=  Place.get_activities(excluding, region).near(first).distinct(:name).slice(0, limit -  others.count)
+    end
+    # If we don't find we don't care about region and categories
+    if others.count < limit - 1
+      others +=  Place.get_activities(excluding).near(first).distinct(:name).slice(0, limit -  others.count)
+    end
     [first, restaurants.first, others, restaurants.last].flatten
   end
 
   # Creates a request to fetch activities based on region and categories
-  def self.get_activities(excluding, region, categories)
+  def self.get_activities(excluding, region = Place.regions.map(&:first), categories = Category.all)
     Place.excluding(excluding).joins(:category).where(region: region).where(categories: categories).where.not(categories: {section: ['lodging', 'restaurant']})
   end
 
--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins ENV.fetch('FRONT_END_URL', 'http://localhost:3001')
     # TODO REMOVE IN PRODUCTION
-    # origins "*"
+    origins "*"
     resource '*',
              headers: :any,
             #  expose: %w[access-token expiry uid client],
